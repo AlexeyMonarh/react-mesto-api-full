c37a6e5f4435352a471ffbee3e842a6c42f25f25
AlexeyMonarh <monarhweb@gmail.com>
2021-02-22 18:17:11 +0300

    Predeploy

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
-
+app.use(cors());
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
-const { Forbidden } = require('../errors');
+const { NotFound } = require('../errors');
 const { JWT_SECRET } = require('../config/index');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    throw new Forbidden('Нет токена!');
+    throw new NotFound('Нет токена!');
   }
   const token = authorization.replace(/^Bearer /, '');
   try {
     const user = jwt.verify(token, JWT_SECRET);
     req.user = user;
   } catch (err) {
-    throw new Forbidden('Нет токена!');
+    throw new NotFound('Нет токена!');
   }
   next();
 };
